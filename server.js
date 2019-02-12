e9d1a7404bf71b3e18b1950e0b9ee0427f0e3cd1
chetan <sidharthaupreti@.com>
2019-02-12 08:55:41 +0530

    3rd

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(server_port || server_host || 8080);
-const path = require('path');
-app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-console.log('Server listening on port 8080');
+const path=require('path');
+
+app.use(express.static(__dirname + '/dist/frontend'));
+
+app.get("/*",(req,res)=>{
+    res.sendFile(path.join(__dirname+'dist/frontend/index.html'))
+})
+
+app.listen(process.env.PORT || 8080,()=>{
+    console.log("server started")
+})
+
 
