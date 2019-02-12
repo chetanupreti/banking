a02215afb2867fc08b229e39b90b277e58858309
chetan <sidharthaupreti@.com>
2019-02-12 09:21:43 +0530

    4th

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
-const path=require('path');
-
 app.use(express.static(__dirname + '/dist/frontend'));
-
-app.get("/*",(req,res)=>{
-    res.sendFile(path.join(__dirname+'dist/frontend/index.html'))
-})
-
 app.listen(process.env.PORT || 8080,()=>{
-    console.log("server started")
-})
+    console.log('Server listening');
+});
+const path = require('path');
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
+});
 
 
